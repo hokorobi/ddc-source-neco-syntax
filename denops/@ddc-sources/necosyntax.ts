d21560dc5953e67ffbc6f6b397b1b62018cad1c5
hokorobi <hokorobi.hokorobi@gmail.com>
2023-02-04 17:20:19 +0900

    Remove unused

--- denops/@ddc-sources/necosyntax.ts
+++ denops/@ddc-sources/necosyntax.ts
@@ -1,8 +1,6 @@
 import {
   BaseSource,
-  DdcOptions,
   Item,
-  SourceOptions,
 } from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
 import {
   Denops,
@@ -19,10 +17,6 @@ export class Source extends BaseSource<Params> {
 
   override async gather(args: {
     denops: Denops;
-    options: DdcOptions;
-    sourceOptions: SourceOptions;
-    sourceParams: Params;
-    completeStr: string;
   }): Promise<Item[]> {
     const words =  await args.denops.call('necosyntax#gather_candidates') as string[];
     return words.map((word) => ({word})) as Item[];
